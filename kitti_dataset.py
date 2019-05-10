86c0685f60077c2d7c02b2cab29d955ee5764373
ClaireLC <clairec@gmail.com>
2019-05-10 05:51:47 +0000

    added ground truth pose and velocity to dataset class

--- kitti_dataset.py
+++ kitti_dataset.py
@@ -95,29 +95,59 @@ class KittiDataset(Dataset):
     diff_im = io.imread(diff_im_path)
 
     # Get ground truth pose from seq_num.txt file
-
+    poses_file_path = self.poses_dir + seq_num_str + ".txt"
+    fid = open(poses_file_path)
+    pose_str = None
+    for i, line in enumerate(fid):
+      if i == frame_num - 1:
+        pose_str = line
+      if i > frame_num - 1:
+        break
+    pose = [float(s) for s in pose_str.split(" ")] 
+    #print(pose)
 
     # Get ground truth velocities from oxts frame_num.txt file
+    oxts_file_digits = 10
+    oxts_file_str = str(frame_num).zfill(oxts_file_digits)
+    oxts_file_path = self.oxts_dir + seq_num_str + "/data/" + oxts_file_str + ".txt"
+    print(oxts_file_path)
     
+    for_vel_line_num = 7
+    ang_vel_line_num = 18
+
+    for_vel = None
+    ang_vel = None
+
+    fid = open(oxts_file_path)
+    pose_str = None
+    line = fid.readlines()
+    oxts_data = [float(s) for s in line[0].split(" ")]
+    for_vel = oxts_data[for_vel_line_num]
+    ang_vel = oxts_data[ang_vel_line_num]
+    print(oxts_data)
+    #print(for_vel, ang_vel)
+
     # Format sample
     sample= {
             "curr_im": curr_im,
             "diff_im": diff_im,
-            #"pose": pose,
-            #"for_vel": for_vel,
-            #"ang_vel": ang_vel,
+            "pose": pose,
+            "for_vel": for_vel,
+            "ang_vel": ang_vel,
             }
+    #print(sample)
 
     return
     
 def main():
-  seq_dir = "/home/clairechen/KITTI/dataset_post/sequences/"
-  poses_dir = "/home/clairechen/KITTI/dataset/poses/"
-  oxts_dir = "/home/clairechen/KITTI/dataset/poses/"
+  seq_dir = "/mnt/disks/dataset/dataset_post/sequences/"
+  poses_dir = "/mnt/disks/dataset/dataset/poses/"
+  oxts_dir = "/mnt/disks/dataset/dataset_post/oxts/"
   dataset = KittiDataset(seq_dir, poses_dir, oxts_dir)
   print(len(dataset))
 
-  sample = dataset[20601]
+  sample = dataset[0]
+  #sample = dataset[46380 - 1]
 
 if __name__ == "__main__":
   main()
