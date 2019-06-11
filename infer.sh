263c3f2aa62dee4d9094ce459e087a0fda839d98
Richard Lin <yifengl@stanford.edu>
2019-06-11 06:52:51 +0000

    infer results added

--- infer.sh
+++ infer.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 dataset="kitti"
-traj_nums=(0 1 2 3 4 5 6 7 8 9)
+#traj_nums=(0 1 2 3 4 5 6 7 8 9)
+traj_nums=(0 1 2 3)
 
 for i in "${traj_nums[@]}"; do
-  python3 cnn_infer.py --traj_num "$i"
+    python3 cnn_infer.py --checkpoint log/normal_cn_forward_300epoch_1.00e-05/checkpoints/2019-06-11_02_48_1.00e-05_bestloss_feed_forward.tar  --save cnn_results/normal_cn_forward_300epoch_1.00e-05  --traj_num "$i"
 done
